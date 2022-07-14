238391d52a10f9a97220a125470fae555caf91e6
Benjent <Benjent@hotmail.fr>
2022-07-14 08:58:17 +0200

    Rename Summary labels for more clarity

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -3,23 +3,23 @@
         <h2 class="title title--2" id="summary">Summary</h2>
         <div v-if="data">
             <div class="data">
-                <label class="data__label">Total</label>
+                <label class="data__label">Total number of reads</label>
                 <output class="data__value">{{data.total}}</output>
             </div>
             <div class="data">
-                <label class="data__label">Single block</label>
+                <label class="data__label">Number of reads with single blocks</label>
                 <output class="data__value">{{data.single_block}}</output>
             </div>
             <div class="data">
-                <label class="data__label">Multiple block count</label>
+                <label class="data__label">Number of reads with multiple blocks</label>
                 <output class="data__value">{{data.multiple_block}}</output>
             </div>
             <div class="data">
-                <label class="data__label">Short multiple block count</label>
+                <label class="data__label">Number of short reads with multiple blocks</label>
                 <output class="data__value">{{data.short_mutliple_block}}</output>
             </div>
             <div class="data">
-                <label class="data__label">Removed reads</label>
+                <label class="data__label">Number of removed reads</label>
                 <output class="data__value">{{data.removed}}</output>
             </div>
         </div>
