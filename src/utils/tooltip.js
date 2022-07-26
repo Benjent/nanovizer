6f243afa2bb32faff8940df8af5140e6227d1865
Benjent <Benjent@hotmail.fr>
2022-07-26 11:44:54 +0200

    Fix tooltip not letting max lolipop hoverable

--- src/utils/tooltip.js
+++ src/utils/tooltip.js
@@ -13,13 +13,17 @@ const setCoordinates = (event, tooltip) => {
     tooltip
         .style('left', (d3.pointer(event)[0] + leftOffset) + 'px')
         .style('top', (d3.pointer(event)[1]) + 'px')
+        .style('width', 'auto')
+        .style('height', 'auto')
 }
 
 const reset = (tooltip) => {
     tooltip
         .style('opacity', 0)
-        .style('left', 0 + 'px') // Prevent from being out of the webap width on window resize
+        .style('left', 0 + 'px') // Prevent from being out of the webapp width on window resize
         .style('top', 0 + 'px')
+        .style('width', 0 + 'px')
+        .style('height', 0 + 'px')
 }
 
 export default {
