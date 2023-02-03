e44209cd461ca164a1f6187a3afdcbe9aa084987
Benjent <Benjent@hotmail.fr>
2023-02-03 13:27:40 +0100

    fix(hooks): enable lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,11 @@ module.exports = {
         "eslint:recommended"
     ],
     "env": {
+        "node": true,
         "vue/setup-compiler-macros": true
     },
     "rules": {
         "no-unused-vars": 1,
+        "vue/multi-word-component-names": 0,
     }
 }
