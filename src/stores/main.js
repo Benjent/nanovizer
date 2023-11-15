203cd52c40a0d0e859b68cc69fb77dacabcbec7f
Benjent <Benjent@hotmail.fr>
2023-11-15 19:58:37 +0100

    feat: use new hiv showcase file with up to date data

--- src/stores/main.js
+++ src/stores/main.js
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia'
 import axios from '../libs/axios'
+import showcaseNew from '../showcase/showcase-hiv.json'
 import showcaseSmall from '../showcase/showcase-hdv.json'
 import showcaseBig from '../showcase/showcase-sars.json'
 
@@ -62,9 +63,11 @@ const useMainStore = defineStore('main', {
         },
         useShowcaseData() {
             const isShowcaseBig = false && Math.random() < 0.5
-            const showcaseResponse = isShowcaseBig ? showcaseBig : showcaseSmall
+            // const showcaseResponse = isShowcaseBig ? showcaseBig : showcaseSmall
+            const showcaseResponse = showcaseNew
 
-            this.fileName = isShowcaseBig ? 'showcase_big' : 'showcase_small'
+            // this.fileName = isShowcaseBig ? 'showcase_big' : 'showcase_small'
+            this.fileName = 'showcase_hiv'
             this.genomeName = 'genome'
             this.genomeSize = isShowcaseBig ? 30000 : 2000
             this.minPosition3 = undefined
